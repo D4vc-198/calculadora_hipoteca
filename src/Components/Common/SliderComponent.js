a8b5128220c9aa523bcde86781bad5a614c2e7cc
d4vc-198 <contactodiegoavc@gmail.com>
2023-12-14 15:38:40 -0600

    slider component added

---
+++ src/Components/Common/SliderComponent.js
@@ -0,0 +1,11 @@
+import { Stack } from "@mui/material"
+
+const SliderComponent = (props) => {
+    const {defaultValue, min, max, label, unit, onChange, amount, value, steps} = props
+
+    return (
+        <Stack my={1.4}></Stack>
+    )
+}
+
+export default SliderComponent
