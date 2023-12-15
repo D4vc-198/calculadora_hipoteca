c6a8ab540f81ba1774709cdb9fa6616c1ec5b900
d4vc-198 <contactodiegoavc@gmail.com>
2023-12-14 22:40:08 -0600

    slider with props added

--- src/Components/Common/SliderComponent.js
+++ src/Components/Common/SliderComponent.js
@@ -1,14 +1,39 @@
-import { Stack, Typography } from "@mui/material"
+import { Slider, Stack, Typography } from "@mui/material";
 
 const SliderComponent = (props) => {
-    const {defaultValue, min, max, label, unit, onChange, amount, value, steps} = props
+  const {
+    defaultValue,
+    min,
+    max,
+    label,
+    unit,
+    onChange,
+    amount,
+    value,
+    steps,
+  } = props;
 
-    return (
-        <Stack gap={1}>
-            <Typography variant="subtitle2">{label}</Typography>
-            <Typography variant="h5">{unit} {amount}</Typography>
-        </Stack>
-    )
-}
+  return (
+    <>
+      <Stack gap={1}>
+        <Typography variant="subtitle2">{label}</Typography>
+        <Typography variant="h5">
+          {unit} {amount}
+        </Typography>
+      </Stack>
+      <Slider
+        min={min}
+        max={max}
+        defaultValue={defaultValue}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        onChange={onChange}
+        value={value}
+        marks
+        step={steps}
+      />
+    </>
+  );
+};
 
-export default SliderComponent
+export default SliderComponent;
