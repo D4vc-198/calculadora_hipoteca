887c947ab5f6f129198fc958b722513b9309c6e5
d4vc-198 <contactodiegoavc@gmail.com>
2023-12-14 22:29:02 -0600

    show label added in sliderComponent

--- src/Components/Common/SliderComponent.js
+++ src/Components/Common/SliderComponent.js
@@ -1,10 +1,13 @@
-import { Stack } from "@mui/material"
+import { Stack, Typography } from "@mui/material"
 
 const SliderComponent = (props) => {
     const {defaultValue, min, max, label, unit, onChange, amount, value, steps} = props
 
     return (
-        <Stack gap={1}></Stack>
+        <Stack gap={1}>
+            <Typography variant="subtitle2">{label}</Typography>
+            <Typography variant="h5">{unit} {amount}</Typography>
+        </Stack>
     )
 }
 
