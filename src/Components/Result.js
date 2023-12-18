a1c048562a7cd5c504fb1f816c02a94ab5454808
d4vc-198 <contactodiegoavc@gmail.com>
2023-12-18 13:06:37 -0600

    Result props added

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -2,7 +2,8 @@ import { ArcElement, Chart, Legend, Tooltip } from "chart.js";
 
 Chart.register(ArcElement, Tooltip, Legend);
 
-const Result = () => {
+const Result = (props) => {
+    const { homeValue, loanAmount, loanTerm, interestRate} = props
   return <p>Result</p>;
 };
 
