17fb1f8fe42021ae2feb6019d105d82e3803dbd6
d4vc-198 <contactodiegoavc@gmail.com>
2023-12-18 13:16:02 -0600

    total calculation

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -3,7 +3,18 @@ import { ArcElement, Chart, Legend, Tooltip } from "chart.js";
 Chart.register(ArcElement, Tooltip, Legend);
 
 const Result = (props) => {
-    const { homeValue, loanAmount, loanTerm, interestRate} = props
+  const { homeValue, loanAmount, loanTerm, interestRate } = props;
+
+  const totalLoanMonths = loanTerm * 12;
+  const interestPerMonth = interestRate / 100 / 12;
+  const monthlyPayment =
+    (loanAmount *
+      interestPerMonth *
+      (1 + interestPerMonth) ** totalLoanMonths) /
+    ((1 + interestPerMonth) ** totalLoanMonths - 1);
+
+    const totalInterestGenerated = monthlyPayment * totalLoanMonths - loanAmount
+
   return <p>Result</p>;
 };
 
